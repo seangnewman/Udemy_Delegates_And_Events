53153ec932e1c981f038804ca3d6125a2aa7ca9d
Sean Newman <seangnewman@msn.com>
2018-09-03 06:38:30 -0700

    Completed Chapter2 exercise 3

---
+++ Chap2_Excercise3/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Chap2_Excercise3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.favoriteMovieLabel = new System.Windows.Forms.Label();
+            this.moviesListBox = new System.Windows.Forms.ListBox();
+            this.adjustGrossButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // favoriteMovieLabel
+            // 
+            this.favoriteMovieLabel.AutoSize = true;
+            this.favoriteMovieLabel.Location = new System.Drawing.Point(23, 13);
+            this.favoriteMovieLabel.Name = "favoriteMovieLabel";
+            this.favoriteMovieLabel.Size = new System.Drawing.Size(82, 13);
+            this.favoriteMovieLabel.TabIndex = 0;
+            this.favoriteMovieLabel.Text = "Favorite Movies";
+            // 
+            // moviesListBox
+            // 
+            this.moviesListBox.FormattingEnabled = true;
+            this.moviesListBox.Location = new System.Drawing.Point(26, 43);
+            this.moviesListBox.Name = "moviesListBox";
+            this.moviesListBox.Size = new System.Drawing.Size(493, 134);
+            this.moviesListBox.TabIndex = 1;
+            // 
+            // adjustGrossButton
+            // 
+            this.adjustGrossButton.Location = new System.Drawing.Point(26, 200);
+            this.adjustGrossButton.Name = "adjustGrossButton";
+            this.adjustGrossButton.Size = new System.Drawing.Size(75, 23);
+            this.adjustGrossButton.TabIndex = 2;
+            this.adjustGrossButton.Text = "Adjust Gross";
+            this.adjustGrossButton.UseVisualStyleBackColor = true;
+            this.adjustGrossButton.Click += new System.EventHandler(this.adjustGrossButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 261);
+            this.Controls.Add(this.adjustGrossButton);
+            this.Controls.Add(this.moviesListBox);
+            this.Controls.Add(this.favoriteMovieLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label favoriteMovieLabel;
+        private System.Windows.Forms.ListBox moviesListBox;
+        private System.Windows.Forms.Button adjustGrossButton;
+    }
+}
+
---
+++ Chap2_Excercise3/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chap2_Excercise3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Movies> movies = new List<Movies>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeMovies();
+            MessageBox.Show(returnDate());
+            MessageBox.Show(returnTime());
+            DisplayMovie();
+        }
+
+        // Using the built-in Action delegate
+        //private delegate void MovieHandler(Movies movie);
+
+        private string returnDate()
+        {
+            Func<DateTime, string> todayGreeting;
+            todayGreeting = returnGreeting;
+            return todayGreeting(DateTime.Now);
+        }
+
+        public string returnTime()
+        {
+            Func<DateTime, string> todayGreeting;
+            todayGreeting = returnTimeGreeting;
+            return todayGreeting(DateTime.Now);
+
+        }
+
+        private string returnGreeting(DateTime greetDate)
+        {
+            return String.Format("Hello!  Today is {0}.", greetDate.ToShortDateString());
+        }
+
+        private string returnTimeGreeting(DateTime greetDate)
+        {
+            return String.Format("Hello!  The time is {0}.", greetDate.ToShortTimeString());
+        }
+
+        private void ListMovie(Movies movie)
+        {
+            moviesListBox.Items.Add(
+                String.Format("Name: {0}\t\tGenre: {1}\tRelease Year {2}\tGross: {3}", movie.Name, movie.Genre, movie.ReleaseYear, movie.Gross.ToString("C"))
+                );
+
+        }
+
+        //private void processMovies(MovieHandler mhandler)
+        private void processMovies(Action<Movies> mhandler)
+        {
+            foreach (Movies movie in movies)
+            {
+                mhandler(movie);
+            }
+        }
+
+
+
+        private void DisplayMovie()
+        {
+            moviesListBox.Items.Clear();
+            processMovies(ListMovie);
+        }
+
+        private void InitializeMovies()
+        {
+            movies.Add(new Movies()
+            {
+                Name = "Blade Runner",
+                Genre = "SciFi",
+                ReleaseYear = "1982",
+                Gross = 11000L
+            });
+            movies.Add(new Movies()
+            {
+                Name = "Star Wars",
+                Genre = "SciFi",
+                ReleaseYear = "1977",
+                Gross = 12000L
+            });
+
+            movies.Add(new Movies()
+            {
+                Name = "Avengers",
+                Genre = "Superhero",
+                ReleaseYear = "2012",
+                Gross = 13000L
+            });
+
+            movies.Add(new Movies()
+            {
+                Name = "Batman",
+                Genre = "Superhero",
+                ReleaseYear = "1966",
+                Gross = 14000L
+            });
+
+            movies.Add(new Movies()
+            {
+                Name = "Batman",
+                Genre = "Superhero",
+                ReleaseYear = "1989",
+                Gross = 15000L
+            });
+
+        }
+        private void adjustGross(Movies movie)
+        {
+            movie.Gross -= 100L;
+
+        }
+        private void adjustGrossButton_Click(object sender, EventArgs e)
+        {
+            processMovies(adjustGross);
+            DisplayMovie();
+        }
+    }
+
+}
